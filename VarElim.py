4439addf9859f7711595d4e202be505c3278d55b
Isaac <ischmidt707@gmail.com>
2021-11-08 19:10:22 -0700

    pwProd

--- VarElim.py
+++ VarElim.py
@@ -23,12 +23,56 @@ class VarElim():
                 pass
 
     def pwProd(self, f1, f2):
+
+        # List out union of variables from each factor
         variables = []
         for v in f1.vars:
             variables.append(v)
         for v in f2.vars:
             if v not in variables:
                 variables.append(v)
+
+        # Generate a new empty truth table
+        newTruTable = {}
+        tableDims = []
+
+        for v in variables:
+            if v.value == "":
+                tableDims.append(len(v.possValues))
+                rows = rows * len(v.possValues)
+            else: tableDims.append(1)
+
+        counters = []
+        for v in tableDims:
+            counters.append(0)
+        for row in range(rows):
+            rowKey = []
+            for v in range(len(counters)):
+                rowKey.append(variables[v].possValues[counters[v]])
+            tuple(rowKey)
+            newTruTable[rowKey] = []
+
+            if(row == rows-1):  # that was the last row, so exit loop
+                break
+
+            i = -1
+            counters[i] += 1  # keep track of truth table values via a list of counters
+            while(counters[i] == tableDims[i]):
+                counters[i] = 0
+                i -= 1
+                counters[i] += 1
+
+            # Fill out values of new truth table, as list items
+            for key in newTruTable:
+                for oldKey, oldValue in {**f1.truTable, **f2.truTable}:  # iterate through both Factor's tables
+                    if all(v in key for v in oldKey):
+                        newTruTable[key].append(oldValue)
+
+                # Multiply list items together to get the new values
+                prod = 1
+                for val in newTruTable[key]:
+                    prod = prod * val
+                newTruTable[key] = prod
 
 
 
